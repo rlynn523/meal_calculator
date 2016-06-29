186f0dc6888cc50030b6a5e9f7371dc45c904197
rlynn523 <ryanlynn523@gmail.com>
2016-06-29 10:26:45 -0400

    edited js syntax

--- meal_calculator.js
+++ meal_calculator.js
@@ -5,17 +5,17 @@ console.log("Meal Calculator!");
 var Diner = function(name, meal){
 	this.name = name,
 	this.meal = meal
-}
+};
 
 var Dish = function(food, price){
 	this.food = food,
 	this.price = price
-}
+};
 var Bill = function(beforeTax, tax, total){
 	this.beforeTax = Math.round(beforeTax * 100) / 100,
 	this.tax = Math.round((.08 * this.beforeTax) * 100) / 100,
 	this.total = Math.round((this.beforeTax +  this.tax) * 100) / 100
-}
+};
 
 //------Dishes------//
 var waffles = new Dish("Waffles", 4.50);
@@ -30,20 +30,20 @@ var ryanMeal = {
 	dishes: [waffles.food, eggs.food, coffee.food],
 	mealPrice: eggs.price + waffles.price + coffee.price
 };
-var ryanBill = new Bill(ryanMeal.mealPrice, bill.tax, bill.total);
+var ryanBill = new Bill(ryanMeal.mealPrice, Bill.tax, Bill.total);
 var ryan = new Diner("Ryan", ryanMeal.dishes);
 
 var lisaMeal = {
 	dishes: [toast.food, eggs.food, tea.food],
 	mealPrice: toast.price + eggs.price + tea.price
 };
-var lisaBill = new Bill(lisaMeal.mealPrice, bill.tax, bill.total);
+var lisaBill = new Bill(lisaMeal.mealPrice, Bill.tax, Bill.total);
 var lisa = new Diner("Lisa", lisaMeal.dishes);
 
 //------Personal Bill for Diners------//
 function personalBill(name, mealPrice){
-	console.log(name + "'s Bill", mealPrice)
-};
+	console.log(name + "'s Bill", mealPrice);
+}
 
 //------Total Bill of Diners------//
 function totalBill(x, y){
